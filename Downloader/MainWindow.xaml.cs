5fd624677796a47344308ead0dbf1dfc39bb8922
foxseSReal <207896979+foxseSReal@users.noreply.github.com>
2025-11-29 22:47:09 +0300

    _link TextBox

--- Downloader/MainWindow.xaml.cs
+++ Downloader/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Downloader
 {
@@ -23,7 +15,7 @@ namespace Downloader
 
         private void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            /*Buraya DoubleClick ile Muzik oynatilacak*/
+            /*Burada DoubleClick ile Muzik oynatilacak*/
 
 
         }
